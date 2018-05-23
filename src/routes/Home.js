812a0d50185ab64dbab4a4b7ff762ab01e4352d4
hwigyeompark <phg5590@gmail.com>
2018-05-23 23:40:28 +0900

    update Home

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,7 +4,14 @@ class Home extends Component{
     render(){
         return(
             <div>
-                <h1></h1>
+                <h1>Greetings</h1>
+                <ul>
+                    <li><a href="/ko">Korean</a></li>
+                    <li><a href="/en">English</a></li>
+                    <li><a href="/portfolio/1">SNS</a></li>
+                    <li><a href="/portfolio/2">Contact</a></li>
+                    <li><a href="/portfolio/3">Todo</a></li>
+                </ul>
             </div>
         );
     }
