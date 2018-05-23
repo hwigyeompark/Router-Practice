c4ac756b8ac488eb7387abe97c39a567cdd59a2f
hwigyeompark <phg5590@gmail.com>
2018-05-23 23:50:33 +0900

    add '/portfolio/Contact, Sns, Todo'

---
+++ src/portfolio/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Contact extends Component{
+    render(){
+        return(
+            <div>
+                <h1>CONTACT</h1>
+                <h1>CONTACT</h1>
+                <h1>CONTACT</h1>
+                <h1>CONTACT</h1>
+            </div>
+        );
+    }
+}
+export default Contact;
---
+++ src/portfolio/Sns.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Sns extends Component{
+    render(){
+        return(
+          <div>
+              <h1>SNS</h1>
+              <h1>SNS</h1>
+              <h1>SNS</h1>
+              <h1>SNS</h1>
+          </div>
+        );
+    }
+}
+export default Sns;
