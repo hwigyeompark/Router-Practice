5026a315914581ce7c9881e7d8aa6ee216da76fa
hwigyeompark <phg5590@gmail.com>
2018-05-23 23:46:56 +0900

    add GreetingEn, Portfolio

---
+++ src/routes/GreetingEn.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class GreetingEn extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Good Morning</h1>
+                <p><a href="/">Home</a></p>
+            </div>
+        );
+    }
+}
+export default GreetingEn;
