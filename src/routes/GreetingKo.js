e7490acbfc32df2ed419ff03c677da479a447611
hwigyeompark <phg5590@gmail.com>
2018-05-23 23:43:34 +0900

    add GreetingKo

---
+++ src/routes/GreetingKo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class GreetingKo extends Component{
+    render(){
+        return(
+            <div>
+                <h1>좋은 아침입니다.</h1>
+                <p><a href="/">홈으로</a></p>
+            </div>
+        )
+    }
+}
+export default GreetingKo;
