9d8e32177be6a57799f65dfd23871dc7187af22a
hwigyeompark <phg5590@gmail.com>
2018-05-23 23:52:19 +0900

    update Portfolio

--- src/routes/Portfolio.js
+++ src/routes/Portfolio.js
@@ -1,1 +1,13 @@
 import React, { Component } from 'react';
+import Sns from '../portfolio/Sns';
+import Contact from  '../portfolio/Contact';
+import Todo from '../portfolio/Todo';
+
+class Portfolio extends Component{
+    render(){
+        return(
+            <div></div>
+        );
+    }
+}
+export default Portfolio;
