4efad7d3f0e2ffc960b99780ad6fe9347fd7c408
hwigyeompark <phg5590@gmail.com>
2018-05-24 23:31:18 +0900

    update Portfolio

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import Home from "./routes/Home";
+import GreetingKo from "./routes/GreetingKo";
+import GreetingEn from "./routes/GreetingEn";
+import Portfolio from "./routes/Portfolio";
 
-import
 
 class App extends Component {
   render() {
     return (
       <div>
+        <BrowserRouter>
+          <div>
+            <Route exact path='/' component={Home} />
+            <Route path='/ko' component={GreetingKo} />
+            <Route path='/en' component={GreetingEn} />
+            <Route path='/portfolio/:id' component={Portfolio} />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
--- src/routes/Portfolio.js
+++ src/routes/Portfolio.js
@@ -4,9 +4,24 @@ import Contact from  '../portfolio/Contact';
 import Todo from '../portfolio/Todo';
 
 class Portfolio extends Component{
+    showPortfolio = id => {
+        switch (parseInt(id)){
+            case 1 : return <Sns/>;
+            case 2 : return <Contact/>;
+            case 3 : return <Todo/>;
+            default : return 'NONE';
+        }
+    };
     render(){
         return(
-            <div></div>
+            <div>
+                <h1>Portfolio</h1>
+                <div>{
+                    this.showPortfolio(this.props.match.params.id)
+                }
+                </div>
+                <p><a href="/">Home</a></p>
+            </div>
         );
     }
 }
